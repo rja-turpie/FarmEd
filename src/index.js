9333d9be04bd72dd9f5beecef28b6bcec043c084
Guneet Sawhney <guneetsawhney:@gmail.com>
2021-04-17 15:41:30 +0100

    Farmer profile page

--- src/index.js
+++ src/index.js
@@ -1,13 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import  Album from './Lan';
+import   LandingPage from './Landingpg';
 import reportWebVitals from './reportWebVitals';
 
 
 ReactDOM.render(
   <React.StrictMode>
-    <Album />
+    <LandingPage />
   </React.StrictMode>,
   document.getElementById('root')
 );
