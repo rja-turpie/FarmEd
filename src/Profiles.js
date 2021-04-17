e6134aa1b92010d1259d5a3f35787d7cfbd33e87
Anna <s1601847@sms.ed.ac.uk>
2021-04-17 21:25:51 +0100

    committed

--- src/Profiles.js
+++ src/Profiles.js
@@ -13,7 +13,7 @@ import Typography from '@material-ui/core/Typography';
 import { makeStyles } from '@material-ui/core/styles';
 import Container from '@material-ui/core/Container';
 import Link from '@material-ui/core/Link';
-import farmer from './farmerview.jpg'
+
 
 function Copyright() {
   return (
