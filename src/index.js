e8353c5f37cc267460b787ebe5d299f368109bed
rja-turpie <rjaturpie@gmail.com>
2021-04-17 16:54:26 +0100

    sedfsfg

--- src/index.js
+++ src/index.js
@@ -1,17 +1,47 @@
+// import React from 'react';
+// import ReactDOM from 'react-dom';
+// import './index.css';
+// import Landingpg from './Landingpg';
+// import reportWebVitals from './reportWebVitals';
+
+
+
+// ReactDOM.render(
+//   <React.StrictMode>
+//     <Landingpg />
+//   </React.StrictMode>,
+//   document.getElementById('root')
+// );
+
+// reportWebVitals();
+
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { BrowserRouter, Route } from 'react-router-dom';
 import './index.css';
-import SignInSide from './SignInSide';
-import reportWebVitals from './reportWebVitals';
+import App from './App2'
+import Landingpg from './Landingpg';
+import SignInSide from './SignInSide'
 
-ReactDOM.render(
-  <React.StrictMode>
-    <SignInSide />
-  </React.StrictMode>,
-  document.getElementById('root')
+
+
+const Routes = () => (
+  <BrowserRouter>
+      <App>
+          <Route exact path="/login" component={SignInSide} />
+          <Route exact path="/" component={Landingpg} />
+      </App>
+  </BrowserRouter>
 );
+
+
+
+ReactDOM.render(<Routes />, document.getElementById('root'));
+
+
+
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+
